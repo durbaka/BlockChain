5200bd55471330f18788be22631540fe6978de67
Your Name <you@example.com>
2019-01-23 23:17:28 +0530

    BlockChain Demo

---
+++ src/Block.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+
+    public Block(String data,String previousHash ) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+    //Calculate new hash based on blocks contents
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+        );
+        return calculatedhash;
+    }
+
+    //Increases nonce value until hash target is reached.
+    public void mineBlock(int difficulty) {
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+}
